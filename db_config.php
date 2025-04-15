feb3591ff8e33bc30f96acae39d758d7927f4ab2
Ishraq Jamil <90216858+IshraqJ22@users.noreply.github.com>
2025-04-15 09:54:16 +0600

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
+$host = ''; // Use your hostname here (Usually localhost)
 $dbname = 'mydigitalprofile';
-$username = 'root';
-$password = 'Xribmssjrx22'; // Replace with your actual password
+$username = ''; // Use your username for database (Usually named root)
+$password = ''; // Replace with your actual password
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
